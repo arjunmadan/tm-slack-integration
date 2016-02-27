4d34d9158eb8beea3641d360ce2e1941788f3969
Arjun Madan <arjunmadan1992@gmail.com>
2016-02-27 12:52:28 -0500

    Before pull

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var request = require('request');
+
 var app = express();
 
 app.get('/',function(req, res){
     var request = {
-        text: req.body.text,
+        text: req.query.text,
     };
     res.send("I got this: "+request.text);
 });
